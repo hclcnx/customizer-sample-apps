58bdedeb212a2f1135e8bf6f6152e219745ecae1
MartinDonnelly <martin.w.donnelly@gmail.com>
2018-10-08 12:42:05 +0100

    spacing tweak

--- helloWorld/helloLCONN.user.js
+++ helloWorld/helloLCONN.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hello " + lconn.homepage.userName + "! ";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello " + lconn.homepage.userName + " ! ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
