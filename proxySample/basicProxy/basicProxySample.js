7446fd5652baa359818d240b2ed7c3a4e58b76d6
Brian Gleeson <brian.gleeson@ie.ibm.com>
2018-10-12 12:21:05 +0100

    Add proxy samples

---
+++ proxySample/basicProxy/basicProxySample.js
@@ -0,0 +1,17 @@
+require(["dojo/request", "dojo/topic", "dojo/domReady!"], function(request, topic) {
+  var proxyApiCall = function() {
+    request('/files/customizer/proxy?reponame=customizer-samples-apps&proxyFile=proxySample/basicProxy/jsonTestAPI.json',
+      {
+        headers: {
+          'customizer-proxy': true,
+          'Content-Type': 'application/json'
+        },
+        handleAs: 'json',
+        method: 'GET',
+      }).then((response) => {
+        dojo.query("span.shareSome-title")[0].textContent=response.title;
+    });
+  }
+
+  proxyApiCall();
+});
