d7df33cb310d94668dd891ff0ae5d311a42362a2
MartinDonnelly <martin.w.donnelly@gmail.com>
2018-11-06 23:41:09 +0000

    Update basicProxySample.js

--- proxySample/basicProxy/basicProxySample.js
+++ proxySample/basicProxy/basicProxySample.js
@@ -1,6 +1,6 @@
 require(["dojo/request", "dojo/topic", "dojo/domReady!"], function(request, topic) {
   var proxyApiCall = function() {
-    request('https://jsonplaceholder.typicode.com/todos/2',
+    request('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone%3Fversion%3D2017-09-21%26text%3Dcrap',
       {
         headers: {
           'Content-Type': 'application/json'
