8712df95a5951a2e068de95fe6a619d6fa614d88
MartinDonnelly <martin.w.donnelly@gmail.com>
2018-11-06 23:15:19 +0000

    Update basicProxySample.js

--- proxySample/basicProxy/basicProxySample.js
+++ proxySample/basicProxy/basicProxySample.js
@@ -1,9 +1,8 @@
 require(["dojo/request", "dojo/topic", "dojo/domReady!"], function(request, topic) {
   var proxyApiCall = function() {
-    request('/files/customizer/proxy?reponame=customizer-sample-apps&proxyFile=proxySample/basicProxy/jsonTestAPI.json',
+    request('https://jsonplaceholder.typicode.com/todos/1',
       {
         headers: {
-          'customizer-proxy': true,
           'Content-Type': 'application/json'
         },
         handleAs: 'json',
