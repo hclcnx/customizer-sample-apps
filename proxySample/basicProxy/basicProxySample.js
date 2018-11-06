739fe923ba2bc3bdbe6166e4f946356c5a0ea354
MartinDonnelly <martin.w.donnelly@gmail.com>
2018-11-06 23:19:18 +0000

    Update basicProxySample.js

--- proxySample/basicProxy/basicProxySample.js
+++ proxySample/basicProxy/basicProxySample.js
@@ -1,6 +1,6 @@
 require(["dojo/request", "dojo/topic", "dojo/domReady!"], function(request, topic) {
   var proxyApiCall = function() {
-    request('https://jsonplaceholder.typicode.com/todos/1',
+    request('https://jsonplaceholder.typicode.com/todos/2',
       {
         headers: {
           'Content-Type': 'application/json'
